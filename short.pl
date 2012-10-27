b471bc9d0b2931939991756edaf628ebce9ca54e
bob walker <bob@randomness.org.uk>
2012-10-27 16:39:24 +0100

    move stuff around

--- short.pl
+++ short.pl
@@ -5,12 +5,13 @@ use warnings;
 use Crypt::Skip32::Base64URLSafe;
 
 my $key    = pack( 'H20', "34671290123419302824" );
-my $cipher = Crypt::Skip32::Base64URLSafe->new($key);
 my $id =  $ARGV[0] or die "I need a value to shorten";
-my $short = shorten($id);
+my $short = shorten($id,$key);
 print "$short\n";
 sub shorten {
     my $idarg = $_[0];
+    my $keyarg = $_[1];
+    my $cipher = Crypt::Skip32::Base64URLSafe->new($keyarg);
     my $shortened = $cipher->encrypt_number_b64_urlsafe($idarg);
     return $shortened;
 };
