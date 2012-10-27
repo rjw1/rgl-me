38641ec18e0b15a2b821bb79bfcf7e3ee9f6fc82
bob walker <bob@randomness.org.uk>
2012-10-27 16:01:06 +0100

    use the right perl and look for soemthing to shorten

--- short.pl
+++ short.pl
@@ -1,11 +1,10 @@
-#!perl
+#! /usr/bin/perl
 #use 5.014;
 use strict;
 use warnings;
-my $id = "2345";
+my $id =  $ARGV[0] or die "I need a value to shorten";
 my $short = shorten($id);
 print "$short\n";
-
 sub shorten {
 return $id;
 };
