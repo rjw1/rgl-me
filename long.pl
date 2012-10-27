fc55ba8198fd72d353b8bd3781baac2753ce9c11
bob walker <bob@randomness.org.uk>
2012-10-27 20:57:38 +0100

    script to return stuff from an encrypted

---
+++ long.pl
@@ -0,0 +1,17 @@
+#! /usr/bin/perl
+#use 5.014;
+use strict;
+use warnings;
+use Crypt::Skip32::Base64URLSafe;
+
+my $key    = pack( 'H20', "34671290123419302824" );
+my $id =  $ARGV[0] or die "I need a value to shorten";
+my $short = shorten($id,$key);
+print "$short\n";
+sub shorten {
+    my $idarg = $_[0];
+    my $keyarg = $_[1];
+    my $cipher = Crypt::Skip32::Base64URLSafe->new($keyarg);
+    my $shortened = $cipher->decrypt_number_b64_urlsafe($idarg);
+    return $shortened;
+};
