cd9d6fe46054facf6078f27c869ce955ea24fcea
bob walker <bob@randomness.org.uk>
2012-10-27 21:14:58 +0100

    return url when given a crypted path

--- long.pl
+++ long.pl
@@ -3,11 +3,20 @@
 use strict;
 use warnings;
 use Crypt::Skip32::Base64URLSafe;
+use DBI;
+my $dbh = DBI->connect("DBI:mysql:database=short;host=localhost",
+                         "short", "short",
+                         {'RaiseError' => 1});
+
+
 
 my $key    = pack( 'H20', "34671290123419302824" );
 my $id =  $ARGV[0] or die "I need a value to shorten";
 my $short = shorten($id,$key);
-print "$short\n";
+my $url = $dbh->selectrow_array("select url from url where id = ?",undef,$short) or die "id doenst exist";
+$dbh->disconnect();
+
+print "$url\n";
 sub shorten {
     my $idarg = $_[0];
     my $keyarg = $_[1];
